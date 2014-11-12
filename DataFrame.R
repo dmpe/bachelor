b2638bc4a42bcca802d7c3c8fdffc4c06ce963b3
JohnMalc <f789gh@yahoo.com>
2014-11-12 20:26:04 +0100

    found data on taiwan GDP education > added and later also renamed description of data frame. No more sad !>)

--- DataFrame.R
+++ DataFrame.R
@@ -1,17 +1,17 @@
-# This file loads necessary R-Scripts, adds additional data from rankings and finally creates data.frame
+# This file loads necessary R-Scripts, adds additional data from rankings 
+# and finally creates data.frame
 
 
 source("GDPdata.R")
 source("Quandl.R")
 
-edb =c(1,3,5,19)
+edb = c(1,3,5,19)
 pst = c(2,3,5,4)
-pupilPerTeachers =c(13.2,13.5,16,18)
-
+pupilPerTeachers = c(13.2,13.5,16,18)
 avarageClassSize = c(35.9,31,38.6,40)
 
-# prvni vec k reseni
-expenditurePerStudent = c(23.4,17.3,23.6, NA )
+# muzu to ale illustrovat 
+expenditurePerStudent = c(23.4,17.3,23.6, 4.62 )
 
 df = data.frame(TigersGDP, edb, pst, pupilPerTeachers, avarageClassSize, expenditurePerStudent)
 df
