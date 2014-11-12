7c0cd12fa1dfe4ee9ff884108e86f49a28f0dfdc
JohnMalc <f789gh@yahoo.com>
2014-11-12 21:55:14 +0100

    some small factor analysis

---
+++ FA.R
@@ -0,0 +1,13 @@
+#Some small analysis using Factor analysis
+library(psych)
+library(GPArotation)
+source("DataFrame.R")
+# http://rtutorialseries.blogspot.de/2011/10/r-tutorial-series-exploratory-factor.html
+novydf = df[,3:4]
+
+#class(df)
+
+corelation = cor(novydf)
+solution <- fa(r = corelation, nfactors = 2, rotate = "oblimin", fm = "minres", SMC=FALSE) 
+solution
+
