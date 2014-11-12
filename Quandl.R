a4582c05aca6710955c4a7a761cd25aa809b13b1
JohnMalc <f789gh@yahoo.com>
2014-11-12 20:42:10 +0100

    get some data from quandl and store them in rfile for later use

--- Quandl.R
+++ Quandl.R
@@ -1,1 +1,13 @@
 library("Quandl")
+
+Quandl.auth("GgnxpyUBXHsyQxqp67bY")
+# be aware it takes 2 !!! calls per 1 request
+
+#PaidDataOnHG = Quandl("SGE/HKGMPMI", trim_start="2014-05-01", trim_end="2014-10-31", authcode="GgnxpyUBXHsyQxqp67bY")
+PaidDataOnHG
+#PaidDataOnSK = Quandl("SGE/KORMPMI", trim_start="2014-05-01", trim_end="2014-10-31", authcode="GgnxpyUBXHsyQxqp67bY")
+PaidDataOnSK
+#PaidDataOnSG = Quandl("SGE/SGPMPMI", trim_start="2014-05-01", trim_end="2014-10-31", authcode="GgnxpyUBXHsyQxqp67bY")
+PaidDataOnSG
+#PaidDataOnTW = Quandl("SGE/TWNMPMI", trim_start="2014-05-01", trim_end="2014-10-31", authcode="GgnxpyUBXHsyQxqp67bY")
+PaidDataOnTW
