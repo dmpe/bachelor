2741dc990b8a31303b712ec9f49b1073e55355cd
John Malc <cincenko@seznam.cz>
2014-11-24 00:12:20 +0100

    Add a folder/file for normalisation and update readme.

---
+++ Normalisation/Scale.R
@@ -0,0 +1,26 @@
+# normalize
+normalized = (x-min(x))/(max(x)-min(x))
+
+# https://stats.stackexchange.com/questions/70801/how-to-normalize-data-to-0-1-range
+# http://howto.commetrics.com/methodology/statistics/normalization/
+# http://www.benetzkorn.com/2011/11/data-normalization-and-standardization/
+
+q = df$ExpenditurePerStudentInGDP
+
+# Z SCORE
+
+te =scale(df$ExpenditurePerStudentInGDP)
+te
+
+# by mela byt stejna -> je
+de = (q-mean(q))/sd(q)
+de
+
+# MIN MAX
+# 0 a 1
+normalized = (q-min(q))/(max(q)-min(q))
+normalized
+
+#mezi -1 and 1
+nor = (q - (max(q)+min(q) /2) ) / ((max(q)-min(q))/2)
+nor
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Bachelor
 
 This repo contains all source code I have used for my bachelor's theses on composite indicators. 
 
-Theses can be download here 
+Thesis can be download here <>
 and please feel free to contact me if needed. 
 
 http://indestat.wordpress.com/
