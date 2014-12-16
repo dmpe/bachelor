463d1a4743dcf0154f976fc85e55344025904e52
John Malc <cincenko@seznam.cz>
2014-12-17 00:18:14 +0100

    messy data -> PCA using prcomp func.

--- MultivariateAnalysis/PCA.R
+++ MultivariateAnalysis/PCA.R
@@ -1,2 +1,11 @@
-summary(pc.cr <-prcomp(df[2:8], scale=TRUE))
 plot(pc.cr)
+
+summary(df)
+cor(df[2:8])
+
+summary(pc.cr <-prcomp(df[2:8],scale=TRUE))
+plot(pc.cr)
+pc.cr$rotation
+screeplot(pc.cr, type="line")
+biplot(pc.cr)
+pc.cr$scores
