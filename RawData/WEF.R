5539c6a3ca5d9372b70c0843cf06cda9349c7997
John Malc <cincenko@seznam.cz>
2015-02-07 20:55:20 +0100

    do not store unneccessary var.

--- RawData/WEF.R
+++ RawData/WEF.R
@@ -4,8 +4,8 @@ library(dplyr)
 library(rJava)
 library(xlsx)
 
-wefexcel.notcleaned <-read.xlsx("RawData/DataSources/GCR_Rankings_2014-2015.xlsx", sheetName = "GCI 2013-2014")
-wef <- wefexcel.notcleaned[4:147,1:3]
+wef <-read.xlsx("RawData/DataSources/GCR_Rankings_2014-2015.xlsx", sheetName = "GCI 2013-2014")
+wef <- wef[4:147,1:3]
 wef <- plyr::rename(wef, c("The.Global.Competitiveness.Index.2014.2015.rankings."="Country", "NA."="Ranking_WEF", "NA..1"  = "Score"))
 wef$Country <- str_trim(wef$Country, side="both")
 
