86d10f6c47ae0ea2797691df018237a4e6bc4eef
John Malc <cincenko@seznam.cz>
2015-02-13 23:39:16 +0100

    read again

--- Imputation/Imputation.R
+++ Imputation/Imputation.R
@@ -2,6 +2,7 @@
 library(mice)
 library(VIM)
 library(mi)
+library(R2WinBUGS)
 # https://stackoverflow.com/questions/25966518/daisy-function-warning-message-nas-introduced-by-coercion
 # to "Read the data in as factor variables instead of characters."
 # http://www.researchgate.net/post/What_is_the_proper_imputation_method_for_categorical_missing_value
@@ -17,19 +18,25 @@ qwr <- joinedDB.5[2:7]
 md.pattern(joinedDB.5[2:7])
 # "Another way to study the pattern involves calculating the number of observations per patterns
 # for all pairs of variables."
-md.pairs(joinedDB.5[2:7])
+# md.pairs(joinedDB.5[2:7])
 
 marginplot(qwr[, c("CompletionRate", "LearningCurveRanking")], col = mdc(1:2), cex = 1.2, cex.lab = 1.2, cex.numbers = 1.3, pch = 19)
 
 # mi
-sd<-mi(joinedDB.5,mi.info(joinedDB.5), n.iter = 10)
+miinfo <- mi.info(joinedDB.5)
+miinfo$imp.formula
+sd<-mi(joinedDB.5, miinfo, n.iter = 100)
 f<-mi.data.frame(sd)
-f$LearningCurveRanking <- round(f$LearningCurveRanking,0)
+converged(joinedDB.5, check = "data")
+f$Ranking_LearningCurve <- round(f$Ranking_LearningCurve,0)
+mi.continuous(Ranking_LearningCurve ~ Ranking_HIndex, data = joinedDB.5)
+plot(sd)
+plot(as.bugs.array(sd@mcmc))
 
 # mice
 print(imp <- mice(joinedDB.5, m=10))
 #stripplot(imp, pch = 20, cex = 1.2)
-fit <- with(imp, lm(LearningCurveRanking ~ Ranking_EDB + Ranking_WEF + JournalArticles))
+fit <- with(imp, lm(Ranking_LearningCurve ~ Ranking_EDB + Ranking_WEF + Ranking_HIndex))
 print(pool(fit))
 round(summary(pool(fit)), 2)
 # densityplot(imp, scales = list(x = list(relation = "free")))
