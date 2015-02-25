c6eb636af44d6c34fcc53ef7da1c7e3c41d4f08b
jm <jm@seznam.cz>
2015-02-25 11:07:53 +0100

    endhance Util packages

--- Util/Install_packages.R
+++ Util/Install_packages.R
@@ -7,11 +7,13 @@ ipak <- function(pkg){
     sapply(pkg, require, character.only = TRUE)
 } 
 
-# usage
+# Installation may take very long time (>15 minutes)
 packages <- c("Quandl", "stringr", "ggplot2", "fpc", "plyr", "reshape2", 
               "RColorBrewer", "scales", "grid", "mclust", "psych", "GPArotation",
               "cluster", "dplyr", "rJava", "rvest", "mice", "corrplot",
-              "xlsx", "RCurl", "Rcpp", "VIM", "data.table", "coda")
+              "xlsx", "RCurl", "Rcpp", "VIM", "data.table", "coda", 
+              "rgl", "R2WinBUGS", "mice", "mi", "NbClust", "clustrd", 
+              "flexclust", "ellipse", "psych", "FactoMineR")
 ipak(packages) 
 
 # Because rJava may print error messages (during or after installation), it is still necessary load 
@@ -19,6 +21,6 @@ ipak(packages)
 # http://www.r-statistics.com/2012/08/how-to-load-the-rjava-package-after-the-error-java_home-cannot-be-determined-from-the-registry/
 # https://stackoverflow.com/questions/3311940/r-rjava-package-install-failing
 
-# Or (it may help) use just < R CMD javareconf >
+# Also see readme. Or (it may help) use just < R CMD javareconf >
 
 
