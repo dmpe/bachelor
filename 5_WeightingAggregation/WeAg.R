61bb86fc6c29e759692807dbd56f8417e5eeed85
John Malc <cincenko@seznam.cz>
2015-03-09 21:31:07 +0100

    created

--- 5_WeightingAggregation/WeAg.R
+++ 5_WeightingAggregation/WeAg.R
@@ -9,9 +9,9 @@ source("4_Normalization/Scale.R")
 
 factor1UnitNormalisation <- factorAn$loadings[,1]^2
 factor2UnitNormalisation <- factorAn$loadings[,2]^2
-factor1UnitNormalisation / sum(factorAn$loadings[,1]^2)
-factor2UnitNormalisation / sum(factorAn$loadings[,2]^2)
-weights.DB7 <- data.frame(factor1UnitNormalisation, factor2UnitNormalisation)
+
+weights.DB7 <- data.frame(Factor1Weight=round(factor1UnitNormalisation / sum(factorAn$loadings[,1]^2),3),
+                          Factor2Weight=round(factor2UnitNormalisation / sum(factorAn$loadings[,2]^2),3))
 
 
 
