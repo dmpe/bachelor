435a5306c2833e4d2c112a73a03915dc157f9754
John Malc <cincenko@seznam.cz>
2015-03-09 12:21:09 +0100

    format table 8 is now with comments too <- e.g. ready Scale to 0-3 is not ready yet

--- 4_Normalization/SampleIOEFreedom.R
+++ 4_Normalization/SampleIOEFreedom.R
@@ -1,32 +1,32 @@
 library(scales)
 
 set.seed(5154)
-source("RawData/DataFrame.R")
-source("RawData/DataFrame_NonScaled.R")
-# source("Imputation/MICE_Imputation.R")
+source("1_RawData/DataFrame.R")
+source("2_Imputation/MICE_Imputation.R")
 
-headDf <- nonScaledDataFrame # this has always 23 obs. 
-headDf$MinMax23Sample <- rescale(nonScaledDataFrame$Freedom_Index_NonScaled, to = c(0,1))
-headDf <- head(headDf[, c("Country", "Freedom_Index_NonScaled", "MinMax23Sample")])
+# Scale IEF's original value for 23 countries
+ief <- nonScaledDataFrame # this has always 23 obs. 
+ief$MinMax23Sample <- rescale(nonScaledDataFrame$Freedom_Index_NonScaled, to = c(0,1))
+ief <- head(ief[, c("Country", "Freedom_Index_NonScaled", "MinMax23Sample")])
 
-freedom_normalisation.test <- freedom_normalisation # always 178 obs.
-freedom_normalisation.test$MinMaxOverall <- rescale(freedom_normalisation$Freedom_Index, to = c(0,1))
-freedom_normalisation.test$ZScoreOverall <- scale(freedom_normalisation$Freedom_Index)
-freedom_normalisation.test <- freedom_normalisation.test[, c("Country", "MinMaxOverall", "RankOverall", "ZScoreOverall")]
-
+# Scale IEF's original values for the whole population
+freedom_population <- freedom_normalisation # always 178 obs.
+freedom_population$MinMaxOverall <- rescale(freedom_normalisation$Freedom_Index, to = c(0,1)) # Min-Max
+freedom_population$ZScoreOverall <- scale(freedom_normalisation$Freedom_Index) # Z-Score
+freedom_population <- freedom_population[, c("Country", "MinMaxOverall", "RankOverall", "ZScoreOverall")]
 # take only 6 countries from the above
-freedom_normalisation.test <- subset(freedom_normalisation.test, Country %in% c("Australia", "Brazil", "Canada", "China", "Chile", "Czech Republic"))
+freedom_population <- subset(freedom_population, Country %in% c("Australia", "Brazil", "Canada", "China", "Chile", "Czech Republic"))
 
-freedom.example <- dplyr::full_join(headDf, freedom_normalisation.test, by = "Country")
-freedom.example <- freedom.example[,c(1,5,2,3,4,6)]
+# Combine freedom_population and ief into one dataset, by Country
+freedom_combined <- dplyr::full_join(ief, freedom_population, by = "Country")
+freedom_combined <- freedom_combined[,c(1,5,2,3,4,6)]
 
-freedom.example$ZScore23Sample <- subset(joinedDB.5, Country %in% c("Australia", "Brazil", "Canada", "China", "Chile", "Czech Republic"))$Freedom_Index
+# Take additionally 5 countries from the 23 sample
+freedom_combined$ZScore23Sample <- subset(joinedDB.5, Country %in% c("Australia", "Brazil", "Canada", "China", "Chile", "Czech Republic"))$Freedom_Index
 
-
-freedom.example <- plyr::arrange(freedom.example, desc(freedom.example$Freedom_Index_NonScaled))
-freedom.example <- freedom.example[,c(1,2,3,4,5,7,6)]
-
-round(freedom.example[,-1], 2)
+freedom_combined <- plyr::arrange(freedom_combined, desc(freedom_combined$Freedom_Index_NonScaled))
+freedom_combined <- freedom_combined[,c(1,2,3,4,5,7,6)]
+freedom_combined <- round(freedom_combined[,-1], 4)
 
 
 
--- 4_Normalization/Scale.R
+++ 4_Normalization/Scale.R
@@ -1,9 +1,8 @@
 library(scales)
 library(preprocessCore)
 
-source("RawData/DataFrame.R")
-source("RawData/DataFrame_NonScaled.R")
-source("Imputation/MICE_Imputation.R")
+source("1_RawData/DataFrame.R")
+source("2_Imputation/MICE_Imputation.R")
 
 
 
@@ -11,11 +10,12 @@ source("Imputation/MICE_Imputation.R")
 normalized = (x-min(x))/(max(x)-min(x))
 
 # https://stats.stackexchange.com/questions/70801/how-to-normalize-data-to-0-1-range
+# https://stackoverflow.com/questions/5294955/how-to-scale-down-a-range-of-numbers-with-a-known-min-and-max-value
+# that is the best
 # http://howto.commetrics.com/methodology/statistics/normalization/
 # http://www.benetzkorn.com/2011/11/data-normalization-and-standardization/
 # http://chimera.labs.oreilly.com/books/1230000000345/ch07.html#_incorporating_scaled_values
 # https://composite-indicators.jrc.ec.europa.eu/?q=content/overview
-q = joinedDB.6$Freedom_Index
 
 # # MIN MAX
 # # 0 a 1
@@ -25,28 +25,24 @@ q = joinedDB.6$Freedom_Index
 # #mezi -1 and 1
 # nor = (q - (max(q)+min(q) /2) ) / ((max(q)-min(q))/2)
 # nor
-# 
-# # Quantile normalization
-# normalize.quantiles(as.matrix(joinedDB.6))
 
 
-nonScaledDataFrame.complete.minmaxscaled <- rescale(as.factor(input.mi.df))
-nonScaledDataFrame.complete.minmaxscaled$Country <- joinedDB.5$Country
-nonScaledDataFrame.complete.minmaxscaled <- nonScaledDataFrame.complete.minmaxscaled[ , c(7,1,2,3,4,5,6)] # Reorder them
+# This is non scaled data frame, containing same values in the joinedDB.6$LearningCurveIndex (only)
+# From column country to row.names
+# https://stackoverflow.com/questions/5555408/convert-the-values-in-a-column-into-row-names-in-an-existing-data-frame-in-r
 
-f <- rescale(nonScaledDataFrame.complete[2:7], to = c(0,1))
+nonScaledDataFrame.complete.test <- nonScaledDataFrame
+nonScaledDataFrame.complete.test$LearningCurve_Index <- joinedDB.6$LearningCurve_Index 
+nonScaledDataFrame.complete.test <- data.frame(nonScaledDataFrame.complete.test[,-1], 
+                                               row.names=nonScaledDataFrame.complete.test[,1])
+# Now scale them to 0 to 3
+joinedDB.7 <- rescale(nonScaledDataFrame.complete.test, to = c(0,3))
 
-sapply(input.mi.df, class)
-class(data.frame(input.mi.out))
+# joinedDB.7 <- joinedDB.7[, c(7,1,2,3,4,5,6)] # Reorder them
 
+# nonScaledDataFrame.complete.minmaxscaled <- rescale(as.factor(input.mi.df))
 
-
-
-
-
-
-
-
+sapply(joinedDB.7, class)
 
 
 
