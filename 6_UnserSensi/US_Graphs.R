d770de3bca6522d209184bdd0fb01a228ca2e241
John Malc <cincenko@seznam.cz>
2015-03-30 17:45:05 +0200

    names + split zscore and min max

--- 6_UnserSensi/US_Graphs.R
+++ 6_UnserSensi/US_Graphs.R
@@ -11,14 +11,13 @@ source("4_Normalization/Scale.R")
 source("3_MultivariateAnalysis/PCAandFA.R")
 source("5_WeightingAggregation/WeAg.R")
 
-df.Original.Imputed
-df.Zscore.Imputed
-
-
-df.Original.MM.FA # Min-MAX + FA weights.
-df.Original.MM.EW # MIn-MAX + EW
-df.Zscore.FA # ZSCORE + FA
-df.Zscore.EW # Zscore EW
+#' For printing purposes
+# df.Original.Imputed
+# df.Zscore.Imputed
+# df.Original.MM.FA # Min-MAX + FA weights.
+# df.Original.MM.EW # MIn-MAX + EW
+# df.Zscore.FA # ZSCORE + FA
+# df.Zscore.EW # Zscore EW
 
 
 df.Original.MM.FA$Country <- rownames(df.Original.MM.FA) 
@@ -26,14 +25,16 @@ df.Original.MM.EW$Country <- rownames(df.Original.MM.EW)
 df.Zscore.FA$Country <- rownames(df.Zscore.FA) 
 df.Zscore.EW$Country <- rownames(df.Zscore.EW) 
 
-zdv <- inner_join(df.Original.MM.FA, df.Original.MM.EW, by= "Country")
-zdv2 <- inner_join(df.Zscore.FA, df.Zscore.EW, by= "Country")
+df.Original.MM.FAEW <- inner_join(df.Original.MM.FA, df.Original.MM.EW, by= "Country")
+df.Original.MM.FAEW.Subset <- subset(df.Original.MM.FAEW, select=c(Country, RankMM.FA, RankMM.EW))
 
-zdvBOTH <- inner_join(zdv, zdv2, by= "Country")
+df.Zscore.FAEW <- inner_join(df.Zscore.FA, df.Zscore.EW, by= "Country")
+df.Zscore.FAEW.Subset <- subset(df.Zscore.FAEW, select=c(Country, RankZS.FA, RankZS.EW))
+
+zdvBOTH <- inner_join(df.Original.MM.FAEW, df.Zscore.FAEW, by= "Country")
 zdvBOTH <- subset(zdvBOTH, select=c(Country, RankMM.FA, RankMM.EW, RankZS.FA, RankZS.EW))
 
 what_long <- melt(zdvBOTH, id="Country")  # convert to long format
-
 what_long$Country[what_long$Country == "United States"] <- "USA"
 what_long$Country[what_long$Country == "United Arab Emirates"] <- "UAE"
 what_long$Country[what_long$Country == "United Kingdom"] <- "UK"
@@ -48,10 +49,51 @@ d <- d + ggtitle("Comparison of different methods") + ylab("Position in Ranking"
 d
 
 
+
+meltingOriginal.MM.FAEW.Subset <- melt(df.Original.MM.FAEW.Subset, id="Country")  # convert to long format
+meltingZscore.FAEW.Subset <- melt(df.Zscore.FAEW.Subset, id="Country")  # convert to long format
+
+meltingZscore.FAEW.Subset$Country[meltingZscore.FAEW.Subset$Country == "United States"] <- "USA"
+meltingZscore.FAEW.Subset$Country[meltingZscore.FAEW.Subset$Country == "United Arab Emirates"] <- "UAE"
+meltingZscore.FAEW.Subset$Country[meltingZscore.FAEW.Subset$Country == "United Kingdom"] <- "UK"
+meltingZscore.FAEW.Subset$Country[meltingZscore.FAEW.Subset$Country == "Czech Republic"] <- "Czech Rep."
+meltingZscore.FAEW.Subset$Country[meltingZscore.FAEW.Subset$Country == "South Africa"] <- "S. Africa"
+
+
+meltingOriginal.MM.FAEW.Subset$Country[meltingOriginal.MM.FAEW.Subset$Country == "United States"] <- "USA"
+meltingOriginal.MM.FAEW.Subset$Country[meltingOriginal.MM.FAEW.Subset$Country == "United Arab Emirates"] <- "UAE"
+meltingOriginal.MM.FAEW.Subset$Country[meltingOriginal.MM.FAEW.Subset$Country == "United Kingdom"] <- "UK"
+meltingOriginal.MM.FAEW.Subset$Country[meltingOriginal.MM.FAEW.Subset$Country == "Czech Republic"] <- "Czech Rep."
+meltingOriginal.MM.FAEW.Subset$Country[meltingOriginal.MM.FAEW.Subset$Country == "South Africa"] <- "S. Africa"
+
+
+
+#' http://stackoverflow.com/questions/17150183/r-plot-multiple-lines-in-one-graph
+me1 <- ggplot(data=meltingOriginal.MM.FAEW.Subset, aes(x=Country, y=value, colour=variable, group = variable))
+me1 <- me1 + geom_line() + geom_point(size = 4, shape=21, fill="white")  # geom_ribbon(aes(ymin=value, ymax=value+1))
+me1 <- me1 + coord_cartesian(ylim = c(0, 25)) + scale_y_continuous(breaks = seq(0, 25, 1))
+me1 <- me1 + ggtitle("Comparison of Min-Max method with weights based on FA/EW/Self") + ylab("Position in Ranking") + xlab("Countries") + labs(color = "We/No methods")
+me1
+
+#' http://stackoverflow.com/questions/17150183/r-plot-multiple-lines-in-one-graph
+me2 <- ggplot(data=meltingZscore.FAEW.Subset, aes(x=Country, y=value, colour=variable, group = variable))
+me2 <- me2 + geom_line() + geom_point(size = 4, shape=21, fill="white")  # geom_ribbon(aes(ymin=value, ymax=value+1))
+me2 <- me2 + coord_cartesian(ylim = c(0, 25)) + scale_y_continuous(breaks = seq(0, 25, 1))
+me2 <- me2 + ggtitle("Comparison of Zscore method with weights based on FA/EW/Self") + ylab("Position in Ranking") + xlab("Countries") + labs(color = "We/No methods")
+me2
+
+
+
+
+
+
+
+
 what_long3 <- melt(zscoreMultiEqual.Weights, id="Country")  # convert to long format
 what_long4 <- melt(zscoreMultiFA.Weights, id="Country")  # convert to long format
 what_long5 <- melt(minMaxMultiEqual.Weights, id="Country")  # convert to long format
 what_long6 <- melt(minMaxMultiFA.Weights, id="Country")  # convert to long format
+
 
 # Var1 is country
 # Var2 is indicator
