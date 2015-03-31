88c4912ab00c95f0de975f25e38341830250086d
John Malc <cincenko@seznam.cz>
2015-03-31 14:44:43 +0200

    wrong. data set -> need to use factor weights.

--- 6_UnserSensi/BackToDetails.R
+++ 6_UnserSensi/BackToDetails.R
@@ -15,41 +15,52 @@ source("6_UnserSensi/BackToDetails.R")
 library(dplyr)
 
 
-testingBacktoDetails <- as.data.frame(zscoreMultiEqual.Weights)
+testingBacktoDetails <- as.data.frame(minMaxMultiEqual.Weights)
 testingBacktoDetails$Country <- rownames(testingBacktoDetails) 
 testingBacktoDetails.p1 <- testingBacktoDetails[,1:3]
 testingBacktoDetails.p1$Country <- rownames(testingBacktoDetails.p1) 
 testingBacktoDetails.p2 <- testingBacktoDetails[,4:7]
 
 # Sum rowwise
-testingBacktoDetails.p2 <- adply(testingBacktoDetails.p2, 1, transform, sumA = sum(LearningCurve_Index, CompletionRate, H_Index))
-testingBacktoDetails.p1 <- adply(testingBacktoDetails.p1, 1, transform, sumB = sum(Unemployment , WEF_Score, Freedom_Index))
+testingBacktoDetails.p2 <- adply(testingBacktoDetails.p2, 1, transform, sumEdu = sum(LearningCurve_Index, CompletionRate_NonScaled, H_Index_NonScaled))
+testingBacktoDetails.p1 <- adply(testingBacktoDetails.p1, 1, transform, sumBuEco = sum(Unemployment_NonScaled, WEF_Score_NonScaled, Freedom_Index_NonScaled))
 
 
-df.testingBacktoDetails <- data.frame(Edu = testingBacktoDetails.p2$sumA, BuEco = testingBacktoDetails.p1$sumB, Country = testingBacktoDetails.p1$Country)
+df.testingBacktoDetails <- data.frame(Edu = testingBacktoDetails.p2$sumEdu, 
+                                      BuEco = testingBacktoDetails.p1$sumBuEco, Country = testingBacktoDetails.p1$Country)
+df.testingBacktoDetails$Country <- as.character(df.testingBacktoDetails$Country)
+
+df.testingBacktoDetails$Country[df.testingBacktoDetails$Country == "United States"] <- "USA"
+df.testingBacktoDetails$Country[df.testingBacktoDetails$Country == "United Arab Emirates"] <- "UAE"
+df.testingBacktoDetails$Country[df.testingBacktoDetails$Country == "United Kingdom"] <- "UK"
+df.testingBacktoDetails$Country[df.testingBacktoDetails$Country == "Czech Republic"] <- "Czech Rep."
+df.testingBacktoDetails$Country[df.testingBacktoDetails$Country == "South Africa"] <- "S. Africa"
+df.testingBacktoDetails$Country[df.testingBacktoDetails$Country == "New Zealand"] <- "N. Zealand"
+# df.testingBacktoDetails$Country[df.testingBacktoDetails$Country == "Switzerland"] <- "Swizerl."
 
 what_long9 <- melt(df.testingBacktoDetails, id="Country")  # convert to long format
 
-what_long3 <- melt(zscoreMultiEqual.Weights, id="Country")  # convert to long format
-what_long4 <- melt(zscoreMultiFA.Weights, id="Country")  # convert to long format
-what_long5 <- melt(minMaxMultiEqual.Weights, id="Country")  # convert to long format
-what_long6 <- melt(minMaxMultiFA.Weights, id="Country")  # convert to long format
+# what_long3 <- melt(zscoreMultiEqual.Weights, id="Country")  # convert to long format
+# what_long4 <- melt(zscoreMultiFA.Weights, id="Country")  # convert to long format
+# what_long5 <- melt(minMaxMultiEqual.Weights, id="Country")  # convert to long format
+# what_long6 <- melt(minMaxMultiFA.Weights, id="Country")  # convert to long format
 
 
 # Var1 is country
 # Var2 is indicator
 # source("http://peterhaschke.com/Code/multiplot.R")
-e1 <- ggplot(data=what_long3, aes(Var1, fill=Var2, weight=value)) + geom_bar()
-e2 <- ggplot(data=what_long4, aes(Var1, fill=Var2, weight=value)) + geom_bar()
-e3 <- ggplot(data=what_long5, aes(Var1, fill=Var2, weight=value)) + geom_bar()
-e4 <- ggplot(data=what_long6, aes(Var1, fill=Var2, weight=value)) + geom_bar()
-grid.arrange(e1, e2,e3,e4, ncol = 2, nrow = 2,  main = "Main title")
-grid.arrange(e1, e2,  main = "Main title")
+# e1 <- ggplot(data=what_long3, aes(Var1, fill=Var2, weight=value)) + geom_bar()
+# e2 <- ggplot(data=what_long4, aes(Var1, fill=Var2, weight=value)) + geom_bar()
+# e3 <- ggplot(data=what_long5, aes(Var1, fill=Var2, weight=value)) + geom_bar()
+# e4 <- ggplot(data=what_long6, aes(Var1, fill=Var2, weight=value)) + geom_bar()
+# grid.arrange(e1, e2,e3,e4, ncol = 2, nrow = 2,  main = "Main title")
+# grid.arrange(e1, e2,  main = "Main title")
 
 
-e9 <- ggplot(data=what_long9, aes(Country, fill=variable, weight=value)) + geom_bar()
+e9 <- ggplot(data=what_long9, aes(reorder(Country, value), fill=variable, weight=value)) + geom_bar()
+e9 <- e9 + ggtitle("Comparison of different weights based on Min-Max norm. method") 
+e9 <- e9 + ylab("Position in Ranking") + xlab("Countries") + labs(color = "Weights")
 e9
-
 # multiplot(e1, e2, e3, e4, cols=2)
 # par(mfrow=c(2,2))
 
