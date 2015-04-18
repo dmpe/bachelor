eb04fa7e686f6b961e1b3f402eadbc64b307ecc5
John Malc <cincenko@seznam.cz>
2015-04-18 11:12:38 +0200

    a bit of formating

--- 4_Normalization/Scale.R
+++ 4_Normalization/Scale.R
@@ -5,14 +5,9 @@ library(Compind)
 set.seed(5154)
 
 
-# ##########################################
-# This is only                             #
-# for Min MAX but now also for ZSCORE too  #
-############################################
-
-# source("1_RawData/DataFrame.R")
-# source("2_Imputation/Imputation.R")
-
+#' ##########################################
+#' This is only                             #
+#' for Min MAX but now also for ZSCORE too  #
 #' http://howto.commetrics.com/methodology/statistics/normalization/
 #' http://www.benetzkorn.com/2011/11/data-normalization-and-standardization/
 #' http://chimera.labs.oreilly.com/books/1230000000345/ch07.html#_incorporating_scaled_values
@@ -20,9 +15,14 @@ set.seed(5154)
 #' https://stackoverflow.com/questions/5352099/how-to-disable-scientific-notation-in-r
 #' https://stackoverflow.com/questions/7303322/apply-function-to-each-column-in-a-data-frame-observing-each-columns-existing-da
 #' https://stackoverflow.com/questions/5555408/convert-the-values-in-a-column-into-row-names-in-an-existing-data-frame-in-r
-#' that is the best
 #' https://stats.stackexchange.com/questions/70801/how-to-normalize-data-to-0-1-range 
 #' https://stackoverflow.com/questions/5294955/how-to-scale-down-a-range-of-numbers-with-a-known-min-and-max-value
+
+############################################
+
+# source("1_RawData/DataFrame.R")
+# source("2_Imputation/Imputation.R")
+
 
 #' For testing 
 #' rescale(nonScaledDataFrame$Unemployment_NonScaled, to = c(0,3))
@@ -49,7 +49,7 @@ rownames(df.Original.MinMax) <- row.names(df.Original.Imputed)
 df.Original.MinMax$Unemployment_NonScaled = ((100-0)*(df.Original.Imputed$Unemployment_NonScaled-max(df.Original.Imputed$Unemployment_NonScaled))/
                                        (min(df.Original.Imputed$Unemployment_NonScaled)-max(df.Original.Imputed$Unemployment_NonScaled))) + 0
 # VELKY PRUSER ? CO STIM TED
-df.Original.MinMax$Freedom_Index_NonScaled <- df.Original$Freedom_Index_NonScaled
+# df.Original.MinMax$Freedom_Index_NonScaled <- df.Original$Freedom_Index_NonScaled
 
 # copy and create new data set with new polarity of unempl.
 # df.Zscore.ImputedUnempCorrect <- df.Zscore.Imputed
@@ -63,10 +63,7 @@ df.Zscore.Imputed$Unemployment <- unemplo$Unemployment_ZscoreNEGATIVE
 #' see documentation with precise formulas
 # normalise_ci(df.Original.Imputed,c(1:3),c("NEG","POS", "POS"), method=2)
 
-# possible, used but not correct
-# testJustSo <- max(df.Original.Imputed$Unemployment_NonScaled) + 
-#  min(df.Original.Imputed$Unemployment_NonScaled) - df.Original.Imputed$Unemployment_NonScaled
-# testJustSo
+
 
 
 ############# To continue, look in 'WeightingAggregation' folder, ->> 'WeAg.R' is required to run
