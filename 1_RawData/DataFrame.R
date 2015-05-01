e39575151bd5f5db6f3aa6ac23f2a95644f6b735
JohnMalc <f789gh@yahoo.com>
2015-05-01 18:57:08 +0200

    move and add formatting/

--- 1_RawData/DataFrame.R
+++ 1_RawData/DataFrame.R
@@ -1,20 +1,21 @@
-# This file loads necessary R-Scripts, adds additional data and finally creates data.frame
-
 library(plyr)
 library(dplyr)
 set.seed(5154)
 
+###########################
 # From R Documentation: source() causes R to accept its input from the named file or URL or connection.  Input is read and
 # parsed from that file until the end of the file is reached, then the parsed expressions are evaluated sequentially in the
 # chosen environment.
+# source("1_RawData/CompletionRate.R")
+# This file loads necessary R-Scripts, adds additional data and finally creates data.frame
+###########################
 
+source("1_RawData/EUI.R")
+source("1_RawData/LearningCurve.R")
 source("1_RawData/Unemplo.R")
 source("1_RawData/FreedomIndex.R")
 source("1_RawData/WEF.R")
-source("1_RawData/LearningCurve.R")
-# source("1_RawData/CompletionRate.R")
 source("1_RawData/HIndex.R")
-source("1_RawData/EUI.R")
 
 
 # df.Zscore <- dplyr::left_join(unemplo, freedom, by = "Country")
@@ -65,9 +66,6 @@ df.Original <- dplyr::left_join(df.Original, hdi, by = "Country")
 df.Original <- plyr::arrange(df.Original, df.Original$Country)
 df.Original <- subset(df.Original, select = c(Country, Unemployment_NonScaled, Freedom_Index_NonScaled, WEF_Score_NonScaled, 
                                               LearningCurve_Index, HDIEducatIndex, H_Index_NonScaled))
-
-# To continue look in 'Imputation' folder, ->> 'MICE_Imputation.R'
-
 
 # http://stackoverflow.com/a/10331131
 sapply(df.Original, class)
