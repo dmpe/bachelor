801ce1276faa2d1346db48d00ee6a1b2835c7c61
dmpe <cincenko@seznam.cz>
2015-06-02 17:13:37 +0200

    add ew deco.

--- 6_UnserSensi/BackToDetails.R
+++ 6_UnserSensi/BackToDetails.R
@@ -56,6 +56,7 @@ BusinessValue <- cbind(df.BackToDetails$BussEcon / (df.BackToDetails$Education +
 df.BackToDetails.table <- data.frame(cbind(df.BackToDetails$Country), EducatValue, BusinessValue)
 df.BackToDetails.table
 
+###########################
 # what_long3 <- melt(zscoreMultiEqual.Weights, id="Country")  # convert to long format
 # what_long4 <- melt(zscoreMultiFA.Weights, id="Country")  # convert to long format
 # what_long5 <- melt(minMaxMultiEqual.Weights, id="Country")  # convert to long format
@@ -71,4 +72,41 @@ df.BackToDetails.table
 # grid.arrange(e1, e2,  main = "Main title")
 # multiplot(e1, e2, e3, e4, cols=2)
 # par(mfrow=c(2,2))
+###########################
+
+
+# This is only for Equal Weighting Results
+
+# df.BackToDetails2 <- as.data.frame(minMaxMultiEqual.Weights)
+# df.BackToDetails2$Country <- rownames(df.BackToDetails) 
+# df.BackToDetails2.p1 <- df.BackToDetails2[, 1:3]
+# df.BackToDetails2.p1$Country <- rownames(df.BackToDetails2.p1) 
+# df.BackToDetails2.p2 <- df.BackToDetails2[, 4:7]
+# 
+# # Sum rowwise
+# df.BackToDetails2.p2 <- adply(df.BackToDetails2.p2, 1, transform, sumEdu = sum(LearningCurve_Index, HDIEducatIndex, H_Index_NonScaled))
+# df.BackToDetails2.p1 <- adply(df.BackToDetails2.p1, 1, transform, sumBuEco = sum(Unemployment_NonScaled, WEF_Score_NonScaled, Freedom_Index_NonScaled))
+# 
+# 
+# df.BackToDetails2 <- data.frame(Education = df.BackToDetails2.p2$sumEdu, 
+#                                BussEcon = df.BackToDetails2.p1$sumBuEco, 
+#                                Country = df.BackToDetails2.p1$Country)
+# 
+# df.BackToDetails2$Country <- as.character(df.BackToDetails2$Country)
+# 
+# df.BackToDetails2$Country[df.BackToDetails2$Country == "United States"] <- "USA"
+# df.BackToDetails2$Country[df.BackToDetails2$Country == "United Arab Emirates"] <- "UAE"
+# df.BackToDetails2$Country[df.BackToDetails2$Country == "United Kingdom"] <- "UK"
+# df.BackToDetails2$Country[df.BackToDetails2$Country == "Czech Republic"] <- "Czech Rep."
+# df.BackToDetails2$Country[df.BackToDetails2$Country == "South Africa"] <- "S. Africa"
+# df.BackToDetails2$Country[df.BackToDetails2$Country == "New Zealand"] <- "N. Zealand"
+# # df.BackToDetails2$Country[df.BackToDetails2$Country == "Switzerland"] <- "Swizerl."
+# 
+# df.meltedBackToDetails2 <- melt(df.BackToDetails2, id = "Country")  # convert to long format
+# 
+# e10 <- ggplot(data = df.meltedBackToDetails2, aes(reorder(Country, value), fill = variable, weight = value)) + geom_bar()
+# e10 <- e10 + coord_cartesian(ylim = c(0, 100)) + scale_y_continuous(breaks = seq(0, 100, 5))
+# e10 <- e10 + ggtitle("Bar chart decomposition of the Attractiveness Index (MM.EW)") + scale_fill_discrete(name = "Dimensions")
+# e10 <- e10 + ylab("Index Value") + xlab("Countries")
+# e10
 
